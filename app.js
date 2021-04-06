4b28bfe6b2eb451625d3703a821b288a9a2ae33f
bacloud14 <bacloud14@gmail.com>
2021-04-06 13:34:42 +0200

    partials & little tweaks

--- app.js
+++ app.js
@@ -3,32 +3,35 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const favicon = require('serve-favicon')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const helmet = require("helmet");
 
 var app = express();
-
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, '/public/images', 'favicon.png')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
